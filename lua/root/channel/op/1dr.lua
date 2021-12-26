dc9034c3dc617b08f869b3f603ef8d955f2d4d0b
Mike Robinson <michael_robinson@live.ca>
2021-12-25 22:47:04 -0500

    operator updates

---
+++ lua/root/channel/op/1dr.lua
@@ -0,0 +1,17 @@
+SCALE = 127
+
+function onReceiveNotify(key, value)
+    CC_NUM = value['1DR'][key]
+end
+
+function onValueChanged(key)
+    if key == 'x' then
+        CC_VAL = math.ceil(self.values[key] * SCALE)
+        -- Debug output
+        print(
+            'CC Number: ', CC_NUM,
+            'CC Value: ', CC_VAL
+        )
+        sendMIDI({ MIDIMessageType.CONTROLCHANGE, CC_NUM, CC_VAL })
+    end
+end
