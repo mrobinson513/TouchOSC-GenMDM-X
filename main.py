ed7d288f7c2466218bfdb8672fa477c643497ad7
Mike Robinson <michael_robinson@live.ca>
2021-12-26 23:56:49 -0500

    lua scripts and the XML reader

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 #import yaml
-import xml.etree.ElementTree as ET
+#import xml.etree.ElementTree as ET
 
-tree = ET.parse("genmdm-x.xml")
-root = tree.getroot()
-print(root.tag)
-for node in root.iter('node'):
-    print(node.tag, node.attrib)
-    for node_elems in node.iter():
-        if "properties" in node_elems.tag:
-            for prop_elems in node_elems.iter("property"):
-                if "node" in prop_elems.tag:
-                    for prop_sub in prop_elems.iter():
-                        print(prop_sub.tag, prop_sub.text)
+xmlfile = "./genmdm-x.xml"
+
+
