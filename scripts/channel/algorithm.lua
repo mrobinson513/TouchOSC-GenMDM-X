faa97c614c644d022430e4094a5971a1ddf192c7
Mike Robinson <mike.robinson@513solutions.ca>
2022-03-04 13:13:04 -0500

    simplify and use onNotify

--- scripts/channel/algorithm.lua
+++ scripts/channel/algorithm.lua
@@ -1,4 +1,4 @@
-function update()
+function onReceiveNotify(key,value)
     boxes = self.children
     algo_num = tonumber(self.tag) + 1
 
@@ -13,18 +13,12 @@ function update()
     algo_map[7] = {"B2","B3","C3","D3"} --ALG6
     algo_map[8] = {"D1","D2","D3","D4"} --ALG7
 
-    for j=1,#algo_map[algo_num] do
-        print(algo_map[algo_num][j])
-        for i=1,#boxes do
-            if boxes[i].name == algo_map[algo_num][j] then
-                print(
-                    "Algorithm", self.tag,
-                    "Active Label:", boxes[i].name
-                )
-                boxes[i].color = Color(1.0, 1.0, 1.0)
-            end
-        end
+    print(unpack(algo_map[algo_num]))
+    for i=1,#boxes do
+            boxes[i].color = Color(1.0,0,0)
+    end
+    
+    for i=1,#algo_map[algo_num] do
+        boxes[algo_map[algo_num][i]].color = Color(1.0, 1.0, 1.0)
     end
 end
-
--- this works, but need a way to "reset" inactive labels.
